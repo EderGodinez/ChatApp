14b0cd13e62941763b39f561e1f7b492b00950a2
Eder Lap <eder.godinez26@gmail.com>
2024-05-03 16:02:19 -0600

    final

--- src/env/environments.ts
+++ src/env/environments.ts
@@ -1,12 +1,15 @@
+
 export const environments={
-  production:true,
-  apiKey: "AIzaSyBwZdIOYZxEgyvGtVzP0inJOQdmS-62aB0",
-  authDomain: "chatapp-7d112.firebaseapp.com",
-  projectId: "chatapp-7d112",
-  storageBucket: "chatapp-7d112.appspot.com",
-  messagingSenderId: "16447672070",
-  appId: "1:16447672070:web:925f3e0ac41738f8c05887",
-  measurementId: "G-BHEF1FB5QJ",
-  API_URL:"https://chatapp-back-aki3.onrender.com"
+  production:false,
+  /*Informacion de Aplicacion de firebase*/
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+  /*API Info*/
+  API_URL:""
 }
 

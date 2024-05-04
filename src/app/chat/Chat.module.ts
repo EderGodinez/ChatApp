01bb4ca084b67dd53a2f1477839ec1bb20ec30ea
Eder Lap <eder.godinez26@gmail.com>
2024-05-03 22:07:37 -0600

    change:conexion a render API

--- src/app/chat/Chat.module.ts
+++ src/app/chat/Chat.module.ts
@@ -1,6 +1,7 @@
 import { APP_INITIALIZER, NgModule, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { environments } from 'src/env/environments';
 const a=localStorage.getItem('user')
 let User:any=''
 let currentUserToken:any=''
@@ -10,7 +11,7 @@ if (a) {
 
 
  currentUserToken =User.stsTokenManager?.accessToken
-  const config: SocketIoConfig = { url: 'http://localhost:80', options: {query:{token:currentUserToken}}, };
+  const config: SocketIoConfig = { url: `${environments.API_URL}`, options: {query:{token:currentUserToken}}, };
 @NgModule({
   imports: [
     SocketIoModule.forRoot(config)
